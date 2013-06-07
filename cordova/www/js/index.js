fefd2e137a5fa8fe0098ae787fa9a7fd044841ec
Lucas Holmquist <lholmqui@redhat.com>
2013-06-07 08:31:37 -0400

    add contacts

--- cordova/www/js/index.js
+++ cordova/www/js/index.js
@@ -1,13 +1,46 @@
-var app = {
+var cordovaApp = {
     initialize: function() {
-        app.bind();
+        cordovaApp.bind();
     },
     bind: function() {
         document.addEventListener('deviceready', this.deviceready, false);
     },
     deviceready: function() {
-        // note that this is an event handler so the scope is that of the event
-        // so we need to call app.report(), and not this.report()
+        $( "a[href='#contact']" ).on( "click", cordovaApp.loadContacts );
         coolapp.init();
+    },
+    loadContacts: function() {
+
+        //lets empty the contact div first
+        $( "#contactsList div" ).empty();
+        //load all contacts
+        var options = new ContactFindOptions(),
+            fields = ["displayName", "name", "emails"],
+            success,
+            error;
+
+        options.multiple=true;
+
+        success = function( contacts ) {
+            var i=0,
+                contactDiv = $( "<div>" ),
+                ul = $( "<ul>" ),
+                li;
+
+            for( i; i < contacts.length; i++ ) {
+                li = $( "<li>" + contacts[ i ].name.formatted + "</li>" );
+                ul.append( li );
+            }
+
+            contactDiv.append( ul );
+            $( "#contactsList" ).append( contactDiv );
+        };
+
+        error = function( error ) {
+            alert( "error" );
+            console.log( error );
+        };
+
+        navigator.contacts.find(fields, success, error, options);
     }
 };
